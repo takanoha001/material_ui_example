654099c762d7060b434709863c889b9f82f240e5
takanoha <takanoha001@gmail.com>
2021-05-15 08:51:43 +0900

    makeStyles hook

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,11 +3,30 @@ import { Button, Typography, Container } from "@material-ui/core";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
 import { Send, KeyboardArrowRightOutlined } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  //css package
+  btn: {
+    fontSize: 20,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 10,
+  },
+});
 
 export default function Notes() {
+  const classes = useStyles();
+
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         component="h2"
         gutterBottom
@@ -15,6 +34,16 @@ export default function Notes() {
       >
         Create a New Note{" "}
       </Typography>
+      <Button
+        className={classes.btn}
+        onClick={() => console.log("clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightOutlined />}
+      >
+        Submit{" "}
+      </Button>
 
       {/* <Button type="submit">Submit</Button>
       <ButtonGroup color="secondary" variant="container">
@@ -34,15 +63,6 @@ export default function Notes() {
         </Button>
       }
       */}
-      <Button
-        onClick={() => console.log("clicked me")}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightOutlined />}
-      >
-        Submit{" "}
-      </Button>
     </Container>
   );
 }
